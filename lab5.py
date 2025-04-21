bf2b0ddb41f8fc505f9c3fa3062765c1e2e274a5
shiraga0912 <siragamurtuzayev@gmail.com>
2025-04-21 01:55:29 -0700

    Create lab5.py

---
+++ lab5.py
@@ -0,0 +1,22 @@
+def musbet_lementlerin_cemi(C):
+    cəm = 0
+    for element in C:
+        if element > 0:
+            cəm += element
+    return cəm
+
+# Nümunə siyahı
+C = [1, -3, 5, -2, 4]
+nəticə = musbet_lementlerin_cemi(C)
+print("Müsbət elementlərin cəmi:", nəticə)
+def musbet_lementlerin_cemi(C):
+    cəm = 0
+    for element in C:
+        if element > 0:
+            cəm += element
+    return cəm
+
+# Nümunə siyahı
+C = [1, -3, 5, -2, 4]
+nəticə = musbet_lementlerin_cemi(C)
+print("Müsbət elementlərin cəmi:", nəticə)
